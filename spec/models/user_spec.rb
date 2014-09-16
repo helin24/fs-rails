c5bbf8ccfd79e3ed189084dca25b1679711f95ce
helin24 <helin.shiah@gmail.com>
2014-09-16 07:45:24 -0500

    email validation with regex

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,24 @@ describe "User" do
 		it { should_not be_valid }
 	end
 
+	describe "when email format is invalid" do 
+		it "should be invalid" do 
+			invalid_emails = %w(email@email email email@some+where.com email.com)
+			invalid_emails.each do |e| 
+				@user.email = e
+				expect(@user).not_to be_valid			
+			end	
+		end
+	end
 
+	describe "when email format is valid" do 
+		it "shoudl be valid" do 
+			valid_emails = %w(email@email.com em_aIL@email.com em.a.i.l@email.com email@e.m.ail.com)
+			valid_emails.each do |e|
+				@user.email = e
+				expect(@user).to be_valid
+			end
+		end
+	end
 
 end
