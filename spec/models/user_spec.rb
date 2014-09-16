0e5bdfe5023ffa1d386031c11954391b54112e0e
helin24 <helin.shiah@gmail.com>
2014-09-16 07:16:42 -0500

    updated user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "User" do 
+	before do 
+		@user = User.new(name: "Example user", email: "example@email.com", password: "password", password_confirmation: "password")
+	end
+
+	subject { @user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
 end
