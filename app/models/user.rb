2f409c0ed7bb71605f7452fb01befb7bb54d956b
helin24 <helin.shiah@gmail.com>
2014-09-16 07:58:18 -0500

    additional check on email validation to prevent duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }	
+
+	before_save { self.email = email.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916120817) do
+ActiveRecord::Schema.define(version: 20140916125211) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20140916120817) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
