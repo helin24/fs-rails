84c0c44e02ac7cfa8faa9ba1b1279839f1d0c5f9
helin24 <helin.shiah@gmail.com>
2014-09-16 07:31:02 -0500

    validates presence of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,14 @@ describe "User" do
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:about) }
+	it { should respond_to(:birthday) }
+
+	describe "when email is not present" do 
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+
+
 end
