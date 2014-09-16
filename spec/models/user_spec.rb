c21dae07b23c32c40a5bae2e74bc6834c11e5340
helin24 <helin.shiah@gmail.com>
2014-09-16 08:24:32 -0500

    starting password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ describe "User" do
 	it { should respond_to(:email) }
 	it { should respond_to(:about) }
 	it { should respond_to(:birthday) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:authenticate) }
 
 	describe "when email is not present" do 
 		before { @user.email = " " }
@@ -46,5 +48,36 @@ describe "User" do
 
 		it { should_not be_valid }
 	end
+
+	describe "when password is not present" do 
+		before do 
+			@user = User.new(name: "example", email: "example@email.com", password: " ", password_confirmation: " ")
+		end
+
+		it { should_not be_valid }
+	end
+
+	describe "when passwords do not match" do 
+		before do 
+			@user.password_confirmation = "mismatch"
+		end
+
+		it { should_not be_valid }
+	end
+
+	describe "return value of authenticate method" do 
+		before do 
+			@user.save
+			let(:found_user) { User.find_by(email: @user.email) }
+		end
+
+		describe "when password is valid" do 
+			it { should_be found_user.authenticate(@user.password) }
+			# expect(:found_user.authenticate("password")).to_eq :found_User
+		end
+
+		describe "when password is invalid" do 
+		end
+ 	end
 
 end
