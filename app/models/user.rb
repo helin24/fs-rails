0d6383adc1485265d346865108aec8ae111f7ffd
helin24 <helin.shiah@gmail.com>
2014-10-06 17:46:53 -0500

    session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	before_save { self.email = email.downcase }
+
+	def self.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def self.digest(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+	
+	private
+		def create_remember_token
+			self.remember_token = User.digest(User.new_remember_token)
+		end
 end
