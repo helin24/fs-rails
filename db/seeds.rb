9d0ba544084e73bd4f6b8a2af4c71fe11e93b7ed
helin24 <helin.shiah@gmail.com>
2014-10-13 16:37:57 -0500

    skill and levels seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require_relative '../app/models/level'
+require_relative '../app/models/skill'
+require 'csv'
+
+Level.delete_all
+Skill.delete_all
+
+Level.create(name: 'Pre-Alpha', organization: 'ISI')
+Level.create(name: 'Alpha', organization: 'ISI')
+Level.create(name: 'Beta', organization: 'ISI')
+Level.create(name: 'Gamma', organization: 'ISI')
+Level.create(name: 'Delta', organization: 'ISI')
+Level.create(name: 'Freestyle 1', organization: 'ISI')
+Level.create(name: 'Freestyle 2', organization: 'ISI')
+Level.create(name: 'Freestyle 3', organization: 'ISI')
+Level.create(name: 'Freestyle 4', organization: 'ISI')
+Level.create(name: 'Freestyle 5', organization: 'ISI')
+Level.create(name: 'Freestyle 6', organization: 'ISI')
+Level.create(name: 'Freestyle 7', organization: 'ISI')
+Level.create(name: 'Freestyle 8', organization: 'ISI')
+Level.create(name: 'Freestyle 9', organization: 'ISI')
+Level.create(name: 'Freestyle 10', organization: 'ISI')
+
+# Individual skills
+CSV.foreach('lib/assets/skills.csv', headers: true) do |row|
+	skill = Skill.create(name: row['skill'])
+  puts row.inspect
+  puts row['skill']
+  puts row['level']
+	skill.level = Level.find_by(name: row['level'])
+	skill.save
+end
