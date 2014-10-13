2a7d7db435281461e4bcb143d55dc31f1ebf1653
helin24 <helin.shiah@gmail.com>
2014-10-13 15:55:35 -0500

    edited migrations and added user skill many to many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }	
 	validates :password, length: {minimum: 6}, if: :password
 
+	has_many :users_skills
+	has_many :skills, through: :users_skills
+
 	before_create :create_remember_token
 	has_secure_password
 
--- db/migrate/20141006221351_add_column_started_skating_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnStartedSkatingToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :started_skating, :date
-  end
-end
