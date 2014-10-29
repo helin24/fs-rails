0ff17fff0c556a154139d0aeb495d837c5b82085
helin24 <helin.shiah@gmail.com>
2014-10-28 20:23:10 -0500

    coaches can edit user notes

--- app/controllers/users_levels_controller.rb
+++ app/controllers/users_levels_controller.rb
@@ -1,15 +1,20 @@
 class UsersLevelsController < ApplicationController
 	def show
-		@users_level = UsersLevel.find_by(level_id: params[:id], user_id: current_user.id)
+		@users_level = UsersLevel.find_by_id(params[:id])
+		user = @users_level.user
+
+		if user != current_user && !current_user.skaters.include?(user)
+			@users_level = UsersLevel.find_by(level: @users_level.level, user: current_user)
+			redirect_to users_level_path(@users_level)
+		end
 	end
 
 	def update
-		puts params
 		@users_level = UsersLevel.find(params[:id])
 		params[:users_skill_progress].each do |users_skill_id, progress|
 			UsersSkill.find(users_skill_id).update(progress: progress)
 		end
 		@users_level.compute
-		redirect_to users_level_path(@users_level.level)
+		redirect_to users_level_path(@users_level)
 	end
 end
--- app/controllers/users_skills_controller.rb
+++ app/controllers/users_skills_controller.rb
@@ -1,8 +1,16 @@
 class UsersSkillsController < ApplicationController
 	def show
-		@users_skill = UsersSkill.find_by(skill_id: params[:id], user_id: current_user.id)
+		@users_skill = UsersSkill.find_by_id(params[:id])
+		user = @users_skill.user
+
+		# redirect if user skill is not current user's or current user's student
+		if user != current_user && !current_user.skaters.include?(user)
+			@users_skill = UsersSkill.find_by(skill: @users_skill.skill, user: current_user)
+			redirect_to users_skill_path(@users_skill)
+		end
+
 		@note = Note.new
-		@public_notes = Skill.find(params[:id]).notes.where(public: 1)
+		@public_notes = @users_skill.skill.notes.where(public: 1)
 			.where.not(notable_id: @users_skill.id)
 	end
 end
