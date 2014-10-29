e1ceca9298700b451c0dfa1bab21551ddf2f7000
helin24 <helin.shiah@gmail.com>
2014-10-29 16:34:52 -0500

    styling header

--- db/migrate/20141020185558_create_test_levels.rb
+++ db/migrate/20141020185558_create_test_levels.rb
@@ -2,7 +2,7 @@ class CreateTestLevels < ActiveRecord::Migration
   def change
     create_table :test_levels do |t|
     	t.string :name
-    	t.string :image
+    	t.string :standard_code
 
       t.timestamps
     end
