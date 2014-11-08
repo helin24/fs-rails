8af459b5b7a5cd75cb8cdc9ebdfe38f15ace95a9
helin24 <helin.shiah@gmail.com>
2014-11-07 18:22:10 -0600

    started utility for cropping pdf images

---
+++ lib/utilities/diagram_selector.rb
@@ -0,0 +1,76 @@
+require 'RMagick'
+include Magick
+
+# pic = ImageList.new("app/assets/images/pictures/test.png")
+
+class DiagramSelector
+	attr_reader :pic
+
+	def initialize(file)
+		# could pass it a specific pdf page
+		# or the entire pdf document and relevant pages
+		# or a list of files
+		@pic = ImageList.new(file)[0]
+	end
+
+	def all_colors
+		colors = {}
+		@pic.each_pixel do |pixel, c, r|
+			color = pixel.to_color
+			if colors[color] == nil
+				colors[color] = 1
+			else
+				colors[color] += 1
+			end
+		end
+		colors
+		# look through every pixel
+		# create hash that will include color=> frequency
+		# for each pixel get color
+		# add to hash
+		# return hash
+	end
+
+	def crop_by_color(color)
+	end
+
+	def color_range(color, grid_size = 100)
+		# find location of color start on column
+		# find location of color start on row
+		row = grid_size
+		col = grid_size
+
+		row_appearances = color_appearance_rows(color, grid_size)
+		col_appearances = color_appearance_cols(color, grid_size)
+		
+	end
+
+	def color_appearance_rows(color, grid_size)
+		row_appearances = []
+		until row > @pic.rows do
+			this_row = []
+			row_pixels = @pic.get_pixels(0, row, @pic.columns, 1)
+			row_pixels.each do |pixel|
+				this_row << (pixel.to_color == color)
+			end
+			row_appearances << this_row
+			row += grid_size
+		end
+		row_appearances
+	end
+
+	def color_appearance_cols(color, grid_size)
+		col_appearances = []
+		until col > @pic.columns do
+			this_col = []
+			col_pixels = @pic.get_pixels(col, 0, 1, @pic.rows)
+			col_pixels.each do |pixel|
+				this_col << (pixel.to_color == color)
+			end
+			col_appearances << this_col
+			col += grid_size
+		end
+		col_appearances
+	end
+
+end
