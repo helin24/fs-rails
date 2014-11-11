e7237be90d826128fb39ed8160ec5909a93740ff
helin24 <helin.shiah@gmail.com>
2014-11-11 00:46:04 -0600

    links added, wondering if there are security concerns here

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,16 @@
+require 'uri'
+
 class Note < ActiveRecord::Base
 	belongs_to :notable, polymorphic: true
 	belongs_to :user
 	belongs_to :author, class_name: "User", foreign_key: "author_id"
+
+	def nice_source
+		links = URI.extract(self.source)
+		text_with_links = "(#{self.source.dup})"
+		links.each do |link|
+			text_with_links.gsub!(link, "<a href='#{link}' target='_blank'>#{link}</a>")
+		end
+		text_with_links
+	end
 end
