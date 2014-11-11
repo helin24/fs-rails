fac273e8b551c74ba56a42350a50802a86e674ec
helin24 <helin.shiah@gmail.com>
2014-11-10 23:40:59 -0600

    updating progress works

--- app/controllers/users_test_levels_controller.rb
+++ app/controllers/users_test_levels_controller.rb
@@ -7,6 +7,14 @@ class UsersTestLevelsController < ApplicationController
 			@users_test_level = UsersTestLevel.find_by(test_level: @users_test_level.test_level, user: current_user)
 			redirect_to users_test_level_path(@users_test_level)
 		end
+	end
 
+	def update
+		@users_test_level = UsersTestLevel.find(params[:id])
+		params[:users_field_move_progress].each do |id, progress|
+			UsersFieldMove.find(id).update(progress: progress)
+		end
+		@users_test_level.compute
+		redirect_to @users_test_level
 	end
 end
