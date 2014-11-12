76aa156fae493cb5b18dc937a4fb9b0ce7bbb175
helin24 <helin.shiah@gmail.com>
2014-11-12 16:17:20 -0600

    level selection works

--- app/assets/javascripts/routine.js
+++ app/assets/javascripts/routine.js
@@ -1,3 +1,35 @@
 $(function() {
-  $( ".draggable" ).draggable();
+
+  $(".group-item").on("click", function() {
+		updateGroup(this);
+  });
+
+	var setInitialGroup = function() {
+		var activeGroup = "Level";
+		var activeId = 1;		
+		selectGroup(activeGroup, activeId);
+	}
+
+	var deselectAll = function() {
+		$(".active-group").removeClass("active-group");
+		$(".active-element").removeClass("active-element");
+	}
+
+	var selectGroup = function(activeGroup, activeId) {
+		deselectAll();
+		$(".group-item#" + activeGroup + "-" + activeId).addClass("active-group");
+		$(".element-item." + activeGroup + "." + activeId).addClass("active-element");
+	}
+
+	var updateGroup = function(listItem) {
+		var text = listItem.id.split('-');
+		activeGroup = text[0];
+		activeId = text[1];
+		selectGroup(activeGroup, activeId);
+	}
+
+	setInitialGroup();
+  $(".draggable").draggable();
+
+
 });
--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -2,7 +2,7 @@ class RoutinesController < ApplicationController
 	def new
 		@user = current_user
 		@routine = Routine.new
-		@all_levels = Level.all + TestLevel.all
+		@all_groups = Level.all + TestLevel.all
 		@all_elements = Skill.all + FieldMove.all
 	end
 
